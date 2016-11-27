a19449d9e91fe4b17461b8f3e47072d887aad1eb
David Ungurean <davidungurean@Davids-MacBook-Air.local>
2016-11-27 02:46:20 +0100

    Initial Commit

---
+++ skiptify/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  skiptify
+//
+//  Created by David Ungurean on 27/11/2016.
+//  Copyright © 2016 David Ungurean. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ skiptify/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  skiptify
+//
+//  Created by David Ungurean on 27/11/2016.
+//  Copyright © 2016 David Ungurean. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
